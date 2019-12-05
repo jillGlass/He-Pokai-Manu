ea2524e2948ab18b077c40ae7f321c247fe3783e
Adam Cowie <cowie.adamj@gmail.com>
2019-12-05 16:28:44 +1300

    Remove header references in App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import { Grommet } from 'grommet'
-
-import { BirdHeader } from './BirdHeader'
-
 const App = () => {
   return (
     <Grommet>
       <h1>React development has begun!</h1>
-      <BirdHeader/>
     </Grommet>
   )
 }
