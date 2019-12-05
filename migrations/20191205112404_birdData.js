1de1d39fecaca25b294bf490cd889ee9cbfe18fe
Adam Cowie <cowie.adamj@gmail.com>
2019-12-05 13:51:20 +1300

    add found boolean into birds migration and tables

--- migrations/20191205112404_birdData.js
+++ migrations/20191205112404_birdData.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('birds', (table) => {
     table.increments('bird_id')
     table.string('name')
+    table.boolean('found')
   })
 }
 
