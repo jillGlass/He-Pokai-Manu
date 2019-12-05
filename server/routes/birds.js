1f62734b5d27c8b7d2285f766426bc9498cbc476
Peter Frederick Robinson <peter.frederick.robinson@gmail.com>
2019-12-06 12:59:17 +1300

    Write api routes to call db functions

--- server/routes/birds.js
+++ server/routes/birds.js
@@ -2,27 +2,38 @@ const express = require('express')
 
 const db = require('../db/db')
 
-const knex = require('../db/knexfile')
+// const knex = require('../db/knexfile')
 
 const router = express.Router()
 
+// get route for calling getBirds (returns array of birds)
 router.get('/', (req, res) => {
-  db.getBirds()
+  return db.getBirds()
     .then(birds => {
-      res.render(birds)
+      res.json(birds)
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+// get route for calling getBird (returns object of individual bird)
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.getBird(id)
+    .then(bird => {
+      res.json(bird)
     })
     .catch(err => {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     }
     )
-}
-)
-
-router.post('/', (req, res) => {
-  console.log('Change Bird to Found - Route')
-  const addData = req.body
-  db.insertNewUser(addData)
-    .then()
-  res.redirect('/')
 })
 
+// post route for changing bird found status to true in db
+router.post('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.foundBird(id)
+})
+
+module.exports = router
