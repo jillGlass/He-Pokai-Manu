ce3afa65d4e72d7aa413b58d383478d0130bf122
Jill Glass <glass.nz@gmail.com>
2019-12-07 10:53:07 +1300

    create found button, renamed Pokai

--- client/components/Perching.jsx
+++ client/components/Perching.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import BirdHeader from './BirdHeader'
 import BirdGrid from './BirdGrid'
-import MainFooter from './MainFooter'
+import FoundBtn from './FoundBtn'
+// import MainFooter from './MainFooter'
 
 const Perching = () => (
 <>
   <BirdHeader />
   <BirdGrid />
-  <MainFooter />
+  {/* <MainFooter /> */}
+  <FoundBtn />
 </>
 )
 
