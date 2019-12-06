76f82838e559b09fee04a57726cf3d40140191fe
Peter Frederick Robinson <peter.frederick.robinson@gmail.com>
2019-12-06 16:00:23 +1300

    Remove grommet element

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Grommet } from 'grommet'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Perching from './Perching'
 import Instructions from './Instructions'
@@ -8,16 +7,14 @@ import BirdInfo from './BirdInfo'
 
 const App = () => {
   return (
-    <Grommet>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Perching}/>
-          <Route exact path='/instructions' component={Instructions}/>
-          <Route exact path='/profile/:id' component={BirdProfile}/>
-          <Route exact path='/profile/:id/info' component={BirdInfo}/>
-        </Switch>
-      </Router>
-    </Grommet>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Perching}/>
+        <Route exact path='/instructions' component={Instructions}/>
+        <Route exact path='/profile/:id' component={BirdProfile}/>
+        <Route exact path='/profile/:id/info' component={BirdInfo}/>
+      </Switch>
+    </Router>
   )
 }
 
