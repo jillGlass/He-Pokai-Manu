09d3b868a11150ac6dc8bf59a5e7c13db05974b9
Jill Glass <glass.nz@gmail.com>
2019-12-07 12:16:32 +1300

    create instructions button with icon

--- client/components/Perching.jsx
+++ client/components/Perching.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import BirdHeader from './BirdHeader'
 import BirdGrid from './BirdGrid'
-import BirdInfoBtn from './BirdInfoBtn'
-import FoundBtn from './FoundBtn'
-import BackBtn from './BackBtn'
-
-// import MainFooter from './MainFooter'
+import MainFooter from './MainFooter'
 
 const Perching = () => (
 <>
   <BirdHeader />
   <BirdGrid />
-  {/* <MainFooter /> */}
-  <FoundBtn />
-  <BirdInfoBtn />
-  <BackBtn />
+  <MainFooter />
+
 </>
 )
 
