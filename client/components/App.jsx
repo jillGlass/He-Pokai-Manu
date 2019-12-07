550c24217c6743b630a17246dfd1ee57ba5e3276
Adam Cowie <cowie.adamj@gmail.com>
2019-12-07 15:28:04 +1300

    Change app to work correctly with birdProfile and BirdInfo WIP

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import Perching from './Perching'
 import Instructions from './Instructions'
-import BirdProfile from './BirdProfile'
-import BirdInfo from './BirdInfo'
+// import BirdProfile from './BirdProfile'
+// import BirdInfo from './BirdInfo'
 
 const App = () => {
   return (
@@ -11,8 +11,8 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Perching}/>
         <Route exact path='/instructions' component={Instructions}/>
-        <Route exact path='/profile/:id' component={BirdProfile}/>
-        <Route exact path='/profile/:id/info' component={BirdInfo}/>
+        {/* <Route exact path='/profile/:id' component={BirdProfile}/>
+        <Route exact path='/profile/:id/info' component={BirdInfo}/> */}
       </Switch>
     </Router>
   )
