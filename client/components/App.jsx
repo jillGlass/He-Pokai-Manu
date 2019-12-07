b62d0a26f8c391eb35823fc92c425aa77b847fb9
Adam Cowie <cowie.adamj@gmail.com>
2019-12-07 15:26:36 +1300

    Change to hashrouter, client side router working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import Perching from './Perching'
-// import Instructions from './Instructions'
+import Instructions from './Instructions'
 import BirdProfile from './BirdProfile'
-// import BirdInfo from './BirdInfo'
+import BirdInfo from './BirdInfo'
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={Perching}/>
-        {/* <Route exact path='/instructions' component={Instructions}/> */}
+        <Route exact path='/instructions' component={Instructions}/>
         <Route exact path='/profile/:id' component={BirdProfile}/>
-        {/* <Route exact path='/profile/:id/info' component={BirdInfo}/> */}
+        <Route exact path='/profile/:id/info' component={BirdInfo}/>
       </Switch>
     </Router>
   )
