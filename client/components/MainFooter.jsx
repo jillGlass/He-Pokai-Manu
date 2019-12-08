ee59d40cb10fbcca5477666e282e5f6d60072ef9
Roman <romanmitch@gmail.com>
2019-12-08 18:32:42 +1300

    wip merging in dev

--- client/components/MainFooter.jsx
+++ client/components/MainFooter.jsx
@@ -5,24 +5,22 @@ import { Link, HashRouter as Router } from 'react-router-dom'
 
 class MainFooter extends React.Component {
   state = {
-    count: 10
+    count: 0
   }
-  updateCount() {
-    this.setState({count: state.count -1}) 
+  updateCount () {
+    this.setState({ count: this.state.count + 1 })
   }
-  }
-  this.props.birds.map(bird => {
-    if (bird.found === true) {
-      updateCount()
-    }
-  }
-  )
 
   render () {
+    this.props.birds.map(bird => {
+      if (bird.found === true) {
+        this.updateCount()
+      }
+    }
+    )
     const count = this.state.count
     const message = `You have found ${count} birds!`
     return (
-
       <Menu fixed='bottom' inverted style = {{ backgroundColor: '#F3A712' }}>
         <Container className="footer" >
           <Menu.Item >{message}
