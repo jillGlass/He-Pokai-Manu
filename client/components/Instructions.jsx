708aa6dce4d56724d152b13296c0e0218ed420ae
Jill Glass <glass.nz@gmail.com>
2019-12-08 20:36:17 +1300

    update instructions to remove header centre button

--- client/components/Instructions.jsx
+++ client/components/Instructions.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link, HashRouter as Router } from 'react-router-dom'
 import BirdHeader from './BirdHeader'
-import SecondaryFooter from './SecondaryFooter'
+
 import BackBtn from './BackBtn'
 
 const Instructions = () => (
     <>
       <BirdHeader />
+      <div className='instructionsWrap'>
         <p className="instructions"> He Pokai-Manu is a treasure-hunt game for birds in Aotearoa. We aim to help younger Kiwis learn more about our bird life in an exciting way, while getting them outside and interested in nature. </p>
         <br></br>
         <p className="instructions">To play, get outside and get bird spotting! Each time you spot a bird that is listed, go into the bird profile and push 'POKAI'. Your found bird will then be added to your inventory of birds.</p>
@@ -17,7 +18,8 @@ const Instructions = () => (
             <BackBtn/>
           </Link>
         </Router>
-        <SecondaryFooter />
+
+      </div>
     </>
 )
 
