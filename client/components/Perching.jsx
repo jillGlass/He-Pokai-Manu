5f62a82f3d1c645e7cf6346b3e89eb7ea66926ba
Adam Cowie <cowie.adamj@gmail.com>
2019-12-08 13:31:07 +1300

    Bring data fetch into App

--- client/components/Perching.jsx
+++ client/components/Perching.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-// import BirdHeader from './BirdHeader'
+import BirdHeader from './BirdHeader'
 import BirdGrid from './BirdGrid'
 import MainFooter from './MainFooter'
 
-const Perching = () => (
-<>
-  {/* <BirdHeader /> */}
-  <BirdGrid />
-  <MainFooter />
-
-</>
-)
+class Perching extends React.Component {
+  render () {
+    console.log(this.props)
+    return (
+      <>
+      <BirdHeader />
+      <BirdGrid birds={this.props}/>
+      <MainFooter />
+      </>
+    )
+  }
+}
 
 export default Perching
