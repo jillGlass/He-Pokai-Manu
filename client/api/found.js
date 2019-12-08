7594b41fb145d508c2e28dc0dd6c564f824d9ac7
Roman Mitch <romanmitch@gmail.com>
2019-12-08 20:00:12 +1300

    Fix api function

--- client/api/found.js
+++ client/api/found.js
@@ -1,6 +1,6 @@
 import request from 'superagent'
 
 export default function found (id) {
-  return request.put(`/api/v1/:${id}`)
+  return request.put(`/api/v1/${id}`)
     .then(res => res.body)
 }
