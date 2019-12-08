b032bcec48f60e65385540a22339af7371418e78
Jill Glass <glass.nz@gmail.com>
2019-12-09 11:16:49 +1300

    Make tally update on pokai click

--- client/components/Perching.jsx
+++ client/components/Perching.jsx
@@ -2,14 +2,36 @@ import React from 'react'
 import BirdHeader from './BirdHeader'
 import BirdGrid from './BirdGrid'
 import MainFooter from './MainFooter'
+import fetch from '../api/birds'
 
 class Perching extends React.Component {
+  state = {
+    birds: [],
+    found: 0
+  }
+
+  componentDidMount () {
+    fetch()
+      .then(birds => {
+        this.setState({
+          birds,
+          found: this.counter(birds)
+        })
+      })
+  }
+
+  counter = (birds) => birds.reduce((found, bird) => {
+    if (bird.found) {
+      found++
+    } return found
+  }, 0)
+
   render () {
     return (
       <>
       <BirdHeader />
       <BirdGrid birds={this.props.birds}/>
-      <MainFooter birds={this.props.birds} found={this.props.found}/>
+      <MainFooter birds={this.props.birds} found={this.state.found}/>
       </>
     )
   }
