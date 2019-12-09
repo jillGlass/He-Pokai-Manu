1286d32fb61cccc8a0c9671a8d177b079ad34fa3
Roman Mitch <romanmitch@gmail.com>
2019-12-09 15:59:37 +1300

    Remove defnct db test, rename serverside route test and file

--- tests/server/db/dbfunctions.test.js
+++ tests/server/db/dbfunctions.test.js
@@ -20,24 +20,6 @@ describe('Bird database function tests', () => {
         expect(actual).toBe(expected)
       })
   })
-  it('get bird returns the details of a single bird', () => {
-    expect.assertions(1)
-
-    const id = 1
-
-    const expected = {
-      name: 'Tui',
-      info: 'Tui are unique to New Zealand and belong to the honey-eater family which means that they feed mainly on nectar from the flowers of native plants. They can be quite aggressive! They will chase other Tui away from food and tasty nectar. Tui are found throughout the North, South and Stewart Islands of New Zealand.',
-      image: 'public/images/californiaquail.jpg/',
-      found: 0
-    }
-
-    return db.getBird(id, testDb)
-      .then(bird => {
-        const actual = bird
-        expect(actual).toEqual(expected)
-      })
-  })
 
   it('foundbirds function changes the boolean value of the selected bird to true', () => {
     expect.assertions(1)
