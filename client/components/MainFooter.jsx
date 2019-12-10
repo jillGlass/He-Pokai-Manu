fcf3139b232ff8b2c2d6d8004c53921593e24853
Adam Cowie <cowie.adamj@gmail.com>
2019-12-11 10:26:13 +1300

    if else added to toggle between grammar for birds found

--- client/components/MainFooter.jsx
+++ client/components/MainFooter.jsx
@@ -2,28 +2,33 @@ import React from 'react'
 import { Menu, Container } from 'semantic-ui-react'
 import InstructionsBtn from './InstructionsBtn'
 import { Link, HashRouter as Router } from 'react-router-dom'
+import BirdProfile from './BirdProfile'
 
 class MainFooter extends React.Component {
-  render () {
-    const number = this.props.found
-    const message = `You have found ${number} birds!`
-    return (
-      <Menu fixed='bottom' inverted style = {{ backgroundColor: '#F3A712' }}>
-        <Container className="footer" >
-          {/* <Menu.Item > */}
-          {message}
-          <br></br>
+toggleMessage = () => {
+  if (this.props.found === 1) {
+    return 'You have found 1 bird!'
+  } else return `You have found ${this.props.found} birds!`
+}
+
+render () {
+  return (
+    <Menu fixed='bottom' inverted style = {{ backgroundColor: '#F3A712' }}>
+      <Container className="footer" >
+       
+        {this.toggleMessage()}
+        <br></br>
             Can you find them all?
-          {/* </Menu.Item> */}
-          <Router>
-            <Link to='/instructions'>
-              <InstructionsBtn />
-            </Link>
-          </Router>
-        </Container>
-      </Menu>
-    )
-  }
+        
+        <Router>
+          <Link to='/instructions'>
+            <InstructionsBtn />
+          </Link>
+        </Router>
+      </Container>
+    </Menu>
+  )
+}
 }
 
 export default MainFooter
