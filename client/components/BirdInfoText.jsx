b0414ccd46fc96c39f145e95c6a57d98cebeb5c3
Adam Cowie <cowie.adamj@gmail.com>
2019-12-10 17:21:16 +1300

    bird status changes color with status level

--- client/components/BirdInfoText.jsx
+++ client/components/BirdInfoText.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 class BirdInfoText extends React.Component {
+  toggleColor (bird) {
+    console.log(bird.category)
+    if (bird.category === 'notThreatened') {
+      return 'notThreatened'
+    } else if (bird.category === 'atRisk') {
+      return 'atRisk'
+    } else return 'threatened'
+  }
+
   render () {
     const { id } = this.props.match.params
     const bird = this.props.birds.find(bird => bird.bird_id === Number(id))
@@ -11,8 +20,8 @@ class BirdInfoText extends React.Component {
           <div className='birdInfoTextName'>{bird.name}</div>
           <div className='birdInfoTextInfo'>{bird.info}</div>
           <br></br>
-          <div className='birdInfoTextStatus'>Conservation Status:</div>
-          <div className='birdInfoTextInfo'>{bird.status}</div>
+          <div className='birdInfoTextInfo'>Conservation Status:</div>
+          <div className={this.toggleColor(bird)}>{bird.status}</div>
         </div>
       </React.Fragment>
     )
