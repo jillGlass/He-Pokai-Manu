1fd794564131ce958301e18ef4b617f566365776
JillGlass <glass.nz@gmail.com>
2020-02-06 20:51:09 +1300

    update knex.js and package.json to deploy to heroku

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -42,11 +42,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     },
     pool: {
       min: 2,
