d67ba070cc2d0f138a07013cf2b564a854235d78
JillGlass <glass.nz@gmail.com>
2020-02-06 21:37:30 +1300

    update knexfile production

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -43,18 +43,11 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      directory: path.join(__dirname, 'migrations')
+      directory: './db/migrations'
     },
-    seeds: {
-      directory: path.join(__dirname, 'seeds')
-    }
-  }
+    useNullAsDefault: true
+  },
 
 }
-
